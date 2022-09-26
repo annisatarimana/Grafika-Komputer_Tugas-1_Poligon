3bcaa217789ad0da36af6a13daf63275cad152c8
annisatarimana <100949353+annisatarimana@users.noreply.github.com>
2022-09-26 23:16:16 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,80 @@
+// PROGRAM MENGGAMBAR POLIGON
+
+#include <windows.h>
+#ifdef _APPLE_
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <stdlib.h>
+
+void trapesium()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 0.5, 0.5);
+
+    glBegin(GL_POLYGON);
+
+    glVertex2f(0.3, 0.4);   // Titik 1
+    glVertex2f(0.8, 0.4);   // Titik 2
+    glVertex2f(0.9, 0.5);   // Titik 3
+    glVertex2f(0.8, 0.6);   // Titik 4
+    glVertex2f(0.3, 0.6);
+    glVertex2f(0.2, 0.5);
+
+    glEnd();
+
+    glColor3f(1.0, 0.0, 0.0);
+    glLineWidth(3.0);
+
+    glBegin(GL_LINES);
+
+    glColor3f(0.5, 1.0, 0.0);
+    glVertex2f(0.3, 0.4);   // Garis 1
+    glVertex2f(0.8, 0.4);
+
+    glColor3f(1.0, 1.0, 0.0);
+    glVertex2f(0.8, 0.4);   // Garis 2
+    glVertex2f(0.9, 0.5);
+
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex2f(0.9, 0.5);   // Garis 3
+    glVertex2f(0.8, 0.6);
+
+    glColor3f(0.0, 1.0, 1.0);
+    glVertex2f(0.8, 0.6);   // Garis 4
+    glVertex2f(0.3, 0.6);
+
+    glColor3f(1.0, 0.7, 0.5);
+    glVertex2f(0.3, 0.6);
+    glVertex2f(0.2, 0.5);
+
+    glColor3f(0.9, 0.5, 0.9);
+    glVertex2f(0.2, 0.5);
+    glVertex2f(0.3, 0.4);
+
+    glEnd();
+
+    glFlush();
+}
+
+void Initialize()
+{
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+int main(int iArgc, char** cppArgv){
+    glutInit(&iArgc, cppArgv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(250, 250);
+    glutInitWindowPosition(200, 200);
+    glutCreateWindow("Point");
+    Initialize();
+    glutDisplayFunc(trapesium);
+    glutMainLoop();
+    return 0;
+}
